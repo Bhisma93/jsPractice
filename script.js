7300fa66d371ca89b7120514573f2dce45c428e6
Bhisma Bakhai <bhisma93@gmail.com>
2018-11-07 07:23:12 -0500

    trying to push

---
+++ script.js
@@ -0,0 +1,10 @@
+var title = 'JavaScript Exercises';
+var name = 'Person Reading This, ';
+var note = 'I hope you have a blessed diwali and a great new year!';
+var replace = name + note;
+
+var exMsg = document.getElementById('greeting');
+exMsg.textContent = replace;
+
+var headTitle = document.getElementById('heading');
+headTitle.textContent = title;
